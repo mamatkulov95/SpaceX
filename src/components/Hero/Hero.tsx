e6c5fd6550839a905141d3e18e1b039cd7733fc3
Ma'rufjon <m.mamatkulov95@gmail.com>
2023-09-01 10:12:47 +0500

    [hero] -Developed here section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,29 @@
+import data from "@/assets/data.json";
 import "./Hero.css";
 
 export default function Hero() {
-  return <div className="hero">Hero</div>;
+  const { rightContent } = data;
+
+  return (
+    <div className="hero">
+      <div className="left-content">
+        <h1 className="left-title">ПУТЕШЕСТВИЕ</h1>
+        <p className="left-title left-title-subcontent">на красную планету</p>
+        <button className="left-btn">
+          <span>Начать путашествие</span>
+        </button>
+      </div>{" "}
+      <div className="right-content">
+        <div className="content-data">
+          {rightContent.map(({ name, number, content }, index) => (
+            <div key={index}>
+              <p>{name}</p>
+              <p className="content-numbers">{number}</p>
+              <p>{content}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
