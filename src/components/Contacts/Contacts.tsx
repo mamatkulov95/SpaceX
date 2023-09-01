85d262cb6b9165f4d6646621cea279c1a28cef02
Ma'rufjon <m.mamatkulov95@gmail.com>
2023-09-01 10:33:03 +0500

    [nav-components] -added additional nav menus

---
+++ src/components/Contacts/Contacts.tsx
@@ -0,0 +1,11 @@
+import "./Contact.css";
+
+export default function Contacts() {
+  return (
+    <div className="contact">
+      <p>ADDRESS: 76B VAZHA-PSHAVELA AVE TBILISI 0186, GEORGIA EMAIL</p>
+
+      <p>E-mail: HELLO@SAGIROV.COM</p>
+    </div>
+  );
+}
---
+++ src/components/Technology/Technology.tsx
@@ -0,0 +1,25 @@
+import "./Technology.css";
+
+export default function Technology() {
+  return (
+    <div className="technology">
+      <h2>
+        SpaceX, founded by Elon Musk, has revolutionized space exploration with
+        key technologies:
+      </h2>
+      <p>
+        SpaceX, founded by Elon Musk, has revolutionized space exploration with
+        key technologies: Reusable Rockets: Falcon 9 and Falcon Heavy rockets
+        with reusable first stages. Merlin Engines: Efficient and reliable
+        engines for various SpaceX rockets. Dragon Spacecraft: Cargo and Crew
+        Dragon for ISS missions. Starship: A fully reusable spacecraft for Moon,
+        Mars, and beyond. Raptor Engines: Propelling Starship and Super Heavy
+        launch system. Fairing Recovery: Technology to recover payload fairings.
+        Autonomous Droneships: For precise rocket stage landings at sea.
+        Starlink: Satellite internet with a vast low Earth orbit constellation.
+        Mars Colonization Vision: Long-term goal of making humanity
+        multi-planetary.
+      </p>
+    </div>
+  );
+}
